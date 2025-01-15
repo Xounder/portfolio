31414c696d4e09baf0dd72e38221a7cf27b5c17f
Xounder <renanfellipe1@hotmail.com>
2025-01-14 23:33:25 -0300

    fixed change-section-menu link button

--- src/components/ChangeSectionMenu/ChangeSectionMenu.tsx
+++ src/components/ChangeSectionMenu/ChangeSectionMenu.tsx
@@ -9,11 +9,11 @@ export const ChangeSectionMenu = ({ icon, alt, onClick }: SectionButtonProps) =>
                 <SectionButton icon={icon} alt={alt} onClick={onClick}/>
             </div>
             <ul className='change-section-menu__items'>
-                <li><a href="">About Me</a></li>
-                <li><a href="">Projects</a></li>
-                <li><a href="">Experiences</a></li>
-                <li><a href="">Certificates/Awards</a></li>
-                <li><a href="">Contact</a></li>
+                <a href=""><li>About Me</li></a>
+                <a href=""><li>Projects</li></a>
+                <a href=""><li>Experiences</li></a>
+                <a href=""><li>Certificates/Awards</li></a>
+                <a href=""><li>Contact</li></a>
             </ul>
         </div>
     );

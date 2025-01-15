dbcfc05290f7e324a29aa74aee2b85a498cc1def
Xounder <renanfellipe1@hotmail.com>
2025-01-14 21:43:03 -0300

    experience-card created

---
+++ src/components/ExperienceCard/ExperienceCard.tsx
@@ -0,0 +1,25 @@
+import './ExperienceCard.css';
+import { Button } from '../Button/Button';
+
+interface ExperienceCardProps {
+    image: string;
+    title: string;
+    description: string;
+    info: [string, string];
+}
+
+export const ExperienceCard = ({ image, title, description, info }: ExperienceCardProps) => {
+    return (
+        <div className='experience-card'>
+            <div className='experience-card__content'>
+                <img src={image} alt={title} />
+                <h3>{title}</h3>
+                <p>{description.length > 120 ? description.substring(0, 120) + '...' : description}</p>
+            </div>
+            <div className='experience-card__info'>
+                <p>{info[0]}</p>
+                <Button text={info[1]} size={[0.5, 1.8]} fontSize={0.7} onClick={() => {}}></Button>
+            </div>
+        </div>
+    );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { Button } from './components/Button/Button.tsx'
-import { ProjectCard } from './components/ProjectCard/ProjectCard.tsx'
+import { ExperienceCard } from './components/ExperienceCard/ExperienceCard.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -12,11 +12,10 @@ createRoot(document.getElementById('root')!).render(
       fontSize={1}
       onClick={() => console.log('clicked')} 
     />
-    <ProjectCard 
-      image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjo0lhwc7Zb7VmLQ1D1ES0K2YkvM5wFr0aAQ&s"  title="Project 1" 
-      description="Body text for whatever you’d like to say. Add main takeaway points, quotes, anecdotes, or even a very very short story. asd asd as dasd a asd asdasd asdasd asda asdas dasd asdas dasdas dasd as das das das dasd asd asd  " 
-      tecnologies={['Java', 'Python']} 
-      link="https://google.com" 
+    <ExperienceCard 
+      image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjo0lhwc7Zb7VmLQ1D1ES0K2YkvM5wFr0aAQ&s"  title="Experience 1" 
+      description="Body text for whatever you’ds like to say. Add main takeaway points, quotes, anecdotes, or even a very very short story. asd asd as dasd a asd asdasd asdasd asda asdas dasd asdas dasdas dasd as das das das dasd asd asd  " 
+      info={['Projeto de Pesquisa', '2021']} 
     />
   </StrictMode>,
 )

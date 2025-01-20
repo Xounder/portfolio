a23e8ddf68cfacace381c2700d8a857bad43ccae
Xounder <renanfellipe1@hotmail.com>
2025-01-19 21:09:27 -0300

    added contact-form media-query

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -9,7 +9,7 @@ export const ContactForm = () => {
             <form action="" method="post">
                 <input type="text" id="name" name="name" placeholder='Name*' required />
                 <input type="email" id="email" name="email" placeholder='Email*' required />
-                <input type="tel" id="phone" name="phone" placeholder='Contact Number*' pattern="\(\d{2}\) \d{4,5}-\d{4}" required />
+                <input type="tel" id="phone" name="phone" placeholder='Contact Number*' title='Preencha este campo com: (99) 99999-9999 ou (99) 9999-9999' pattern="\(\d{2}\) \d{4,5}-\d{4}" required />
                 <textarea id="message" name="message" placeholder='Message*' required></textarea>
                 <Button text="Submit" size={[0.6, 6]} fontSize={0.9} type="submit"onClick={() => {}}></Button>
             </form>

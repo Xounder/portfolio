4208b277f2086b366a474d7570fe1de48d763402
Xounder <renanfellipe1@hotmail.com>
2025-01-21 19:45:06 -0300

    added limit visible tecnologies to project-card

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -10,17 +10,36 @@ interface ProjectCardProps {
 }
 
 export const ProjectCard = ({ cardInfo: { image, title, description }, tecnologies, link }: ProjectCardProps) => {
+    const VISIBLE_LIMIT = 6;
+    const visibleTecnologies = tecnologies.slice(0, VISIBLE_LIMIT);
+    const hiddenTecnologies = tecnologies.slice(VISIBLE_LIMIT);
+
     return (
         <a className="project-card" href={link} target="_blank" rel="noopener noreferrer">
-            <DefaultCard 
-                image={image}
-                title={title} 
-                description={description}
-            />
-            <ul className='project-card__tecnologies'>
-                {tecnologies.map((tecnology, index) => (
-                    <li key={index}><Button text={tecnology} size={[0.6, 1.3]} fontSize={0.7} type='button' onClick={() => {}}></Button></li>
+            <DefaultCard image={image} title={title} description={description} />
+            <ul className="project-card__tecnologies">
+                {visibleTecnologies.map((tecnology, index) => (
+                    <li key={index}>
+                        <Button
+                            text={tecnology}
+                            size={[0.6, 1.3]}
+                            fontSize={0.7}
+                            type="button"
+                            onClick={() => {}}
+                        />
+                    </li>
                 ))}
+                {hiddenTecnologies.length > 0 && (
+                    <li>
+                        <Button
+                            text={`More ${hiddenTecnologies.length}`}
+                            size={[0.6, 1.3]}
+                            fontSize={0.7}
+                            type="button"
+                            onClick={() => {}} // Mostrar todas as tecnologias
+                        />
+                    </li>
+                )}
             </ul>
         </a>
     );

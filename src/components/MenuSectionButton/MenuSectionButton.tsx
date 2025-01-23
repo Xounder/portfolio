40c69e67a06c45a51e33879805e2b8cf211d51d9
Xounder <renanfellipe1@hotmail.com>
2025-01-23 18:28:19 -0300

    modified section-butttons

--- src/components/MenuSectionButton/MenuSectionButton.tsx
+++ src/components/MenuSectionButton/MenuSectionButton.tsx
@@ -1,19 +1,22 @@
 import './MenuSectionButton.css';
 
-import { DefaultSectionButton, DefaultSectionButtonProps } from '../DefaultSectionButton/DefaultSectionButton';
+import { DefaultSectionButton } from '../DefaultSectionButton/DefaultSectionButton';
+import { useState } from 'react';
 
-export const MenuSectionButton = ({ icon, alt, onClick }: DefaultSectionButtonProps) => {
+export const MenuSectionButton = ({ icon, alt}: { icon: string, alt: string}) => {
+    const [isActive, setIsActive] = useState(false);
+
     return (
         <div className='menu-section'>
-            <ul className='menu-section__items'>
+            {isActive && (<ul className='menu-section__items'>
                 <a href=""><li>About Me</li></a>
                 <a href=""><li>Projects</li></a>
                 <a href=""><li>Experiences</li></a>
                 <a href=""><li>Certificates/Awards</li></a>
                 <a href=""><li>Contact</li></a>
-            </ul>
+            </ul>)}
             <div className='menu-section__button'>
-                <DefaultSectionButton icon={icon} alt={alt} onClick={onClick}/>
+                <DefaultSectionButton icon={icon} alt={alt} onClick={() => setIsActive(!isActive)}/>
             </div>
         </div>
     );

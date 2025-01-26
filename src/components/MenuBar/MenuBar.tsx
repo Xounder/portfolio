d898be9c0c70d1e6d1f4a893139700410d1a11f0
Xounder <renanfellipe1@hotmail.com>
2025-01-25 21:50:24 -0300

    added links to menu-bar social-media icons

--- src/components/MenuBar/MenuBar.tsx
+++ src/components/MenuBar/MenuBar.tsx
@@ -3,26 +3,40 @@ import './MenuBar.css';
 import logo from '../../assets/logo.png';
 
 type MenuBarProps = {
-    icons: Icons[]
+    sectionName: string;
+    icons: Icon[]
 };
 
-type Icons = {
+export type Icon = {
     icon:string;
     alt:string;
+    link?:string;
 }
 
-export const MenuBar = ({icons}: MenuBarProps) => {
+export const MenuBar = ({sectionName, icons}: MenuBarProps) => {
+    const renderIcons = () => {
+        return icons.map((icon, index) => {
+            const content = icon.link ? (
+                <a href={icon.link} target="_blank" rel="noopener noreferrer">
+                    <img src={icon.icon} alt={icon.alt} />
+                </a>
+            ) : (
+                <img src={icon.icon} alt={icon.alt} />
+            );
+    
+            return <li key={index}>{content}</li>;
+        });
+    };
+
     return (
         <>
             <div className='atual-section'>
                 <div className='atual-section__name'>
                     <img src={logo} alt="" />
-                    <h2>About Me</h2>
+                    <h2>{sectionName}</h2>
                 </div>
                 <ul className='atual-section__icons'>
-                    {icons.map((icon, index) => (
-                        <li key={index}><img src={icon.icon} alt={icon.alt}/></li>
-                    ))}
+                    {renderIcons()}
                 </ul>
             </div>
         </>

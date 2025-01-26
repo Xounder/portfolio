aa10fe7ced0b974e7c0653d3ccc208b668df7979
Xounder <renanfellipe1@hotmail.com>
2025-01-25 22:54:59 -0300

    refactored menu-section-button

--- src/components/MenuSectionButton/MenuSectionButton.tsx
+++ src/components/MenuSectionButton/MenuSectionButton.tsx
@@ -3,20 +3,26 @@ import './MenuSectionButton.css';
 import { DefaultSectionButton } from '../DefaultSectionButton/DefaultSectionButton';
 import { useState } from 'react';
 
-export const MenuSectionButton = ({ icon, alt}: { icon: string, alt: string}) => {
+export const MenuSectionButton = ({ icon, alt, setCurrentSection }: { icon: string, alt: string, setCurrentSection: React.Dispatch<React.SetStateAction<string>> }) => {
     const [isActive, setIsActive] = useState(false);
+
+    const changeCurrentSection = (sectionName: string) => {
+        setCurrentSection(sectionName);
+        setIsActive(false);
+    }
 
     return (
         <div className='menu-section'>
             {isActive && (<ul className='menu-section__items'>
-                <a href=""><li>About Me</li></a>
-                <a href=""><li>Projects</li></a>
-                <a href=""><li>Experiences</li></a>
-                <a href=""><li>Certificates/Awards</li></a>
-                <a href=""><li>Contact</li></a>
+                <button onClick={() => changeCurrentSection('About Me')}><li>About Me</li></button>
+                <button onClick={() => changeCurrentSection('Projects')}><li>Projects</li></button>
+                <button onClick={() => changeCurrentSection('Experiences')}><li>Experiences</li></button>
+                <button onClick={() => changeCurrentSection('Recognitions')}><li>Certificates/Awards</li></button>
+                <button onClick={() => changeCurrentSection('Contact')}><li>Contact</li></button>
+                <button onClick={() => setIsActive(!isActive)}><li>Close</li></button>
             </ul>)}
             <div className='menu-section__button'>
-                <DefaultSectionButton icon={icon} alt={alt} onClick={() => setIsActive(!isActive)}/>
+                <DefaultSectionButton icon={icon} alt={alt} onClick={() => setIsActive(!isActive)} />
             </div>
         </div>
     );

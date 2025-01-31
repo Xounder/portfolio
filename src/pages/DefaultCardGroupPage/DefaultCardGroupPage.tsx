7fe1b39f6dd287dd672977127d9411c764bc8a6d
Xounder <renanfellipe1@hotmail.com>
2025-01-31 03:48:49 -0300

    refactored card-group

--- src/pages/DefaultCardGroupPage/DefaultCardGroupPage.tsx
+++ src/pages/DefaultCardGroupPage/DefaultCardGroupPage.tsx
@@ -2,69 +2,64 @@ import { useRef } from "react";
 import "./DefaultCardGroupPage.css";
 
 import { DefaultCard } from "../../components/DefaultCard/DefaultCard";
-
 import { ProjectCardInfo } from "../../components/ProjectCardInfo/ProjectCardInfo";
-import { ProjectsContent } from "../../data/data";
-import { ProjectsCards } from "../../data/data";
-
 import { ExperienceCardInfo } from "../../components/ExperienceCardInfo/ExperienceCardInfo";
-import { ExperiencesContent } from "../../data/data";
-import { ExperiencesCards } from "../../data/data";
 
-import { RecognitionsContent } from "../../data/data";
-import { RecognitionsCards } from "../../data/data";
+import { ProjectsCards, ProjectsContent } from "../../data/data";
+import { ExperiencesCards, ExperiencesContent } from "../../data/data";
+import { RecognitionsCards, RecognitionsContent } from "../../data/data";
 
-export const DefaultCardGroupPage = ({cardType} : {cardType: string}) => {
-    const scrollRef = useRef<HTMLDivElement>(null);
+const cardDataMap = {
+  Projects: {
+    cards: ProjectsCards,
+    content: ProjectsContent,
+    component: (index: number) => (
+      <ProjectCardInfo tecnologies={ProjectsContent[index].tecnologies} />
+    ),
+  },
+  Experiences: {
+    cards: ExperiencesCards,
+    content: ExperiencesContent,
+    component: (index: number) => (
+      <ExperienceCardInfo info={ExperiencesContent[index].info} />
+    ),
+  },
+  Recognitions: {
+    cards: RecognitionsCards,
+    content: RecognitionsContent,
+    component: (index: number) => (
+      <ExperienceCardInfo info={RecognitionsContent[index].info} />
+    ),
+  },
+};
 
-    const handleScroll = (event: React.WheelEvent) => {
-      if (scrollRef.current) {
-        scrollRef.current.scrollTop += event.deltaY;
-      }
-    };
+export const DefaultCardGroupPage = ({ cardType }: { cardType: string }) => {
+  const scrollRef = useRef<HTMLDivElement>(null);
 
-    return (
-        <>
-            <div className="group-cards" ref={scrollRef} onWheel={handleScroll}>
-                {cardType === "Projects" && ProjectsCards.map((card, index) => (
-                    <DefaultCard 
-                    image= {{
-                        img: card.image.img,
-                        alt: card.image.alt
-                    }}
-                    title= {card.title}
-                    description= {card.description}
-                    link= {card.link}
-                    component={<ProjectCardInfo tecnologies={ProjectsContent[index].tecnologies}/>}
-                    />
-                ))}
+  const handleScroll = (event: React.WheelEvent) => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollTop += event.deltaY;
+    }
+  };
 
-                {cardType === "Experiences" && ExperiencesCards.map((card, index) => (
-                    <DefaultCard 
-                    image= {{
-                        img: card.image.img,
-                        alt: card.image.alt
-                    }}
-                    title= {card.title}
-                    description= {card.description}
-                    link= {card.link}
-                    component={<ExperienceCardInfo info={ExperiencesContent[index].info}/>}
-                    />
-                ))}
+  const selectedData = cardDataMap[cardType as keyof typeof cardDataMap];
 
-                {cardType === "Recognitions" && RecognitionsCards.map((card, index) => (
-                    <DefaultCard 
-                    image= {{
-                        img: card.image.img,
-                        alt: card.image.alt
-                    }}
-                    title= {card.title}
-                    description= {card.description}
-                    link= {card.link}
-                    component={<ExperienceCardInfo info={RecognitionsContent[index].info}/>}
-                    />  
-                ))}
-            </div>
-        </>
-    );
-}
+  return (
+    <div className="group-cards" ref={scrollRef} onWheel={handleScroll}>
+      {selectedData &&
+        selectedData.cards.map((card, index) => (
+          <DefaultCard
+            key={index}
+            image={{
+              img: card.image.img,
+              alt: card.image.alt,
+            }}
+            title={card.title}
+            description={card.description}
+            link={card.link}
+            component={selectedData.component(index)}
+          />
+        ))}
+    </div>
+  );
+};

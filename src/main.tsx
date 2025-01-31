c1b3bd9cde27ec58ed55d9757d5e886e325e39d2
Xounder <renanfellipe1@hotmail.com>
2025-01-30 23:36:56 -0300

    added media-query to info-card and home-page-scroll

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 
-
+import { HomePage } from './pages/HomePage/HomePage'
 import { HomePageScroll } from './pages/HomePageScroll/HomePageScroll'
 
 createRoot(document.getElementById('root')!).render(

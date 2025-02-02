d9e9768bef72a1f203521979c16973b6e892335a
Xounder <renanfellipe1@hotmail.com>
2025-02-02 07:53:45 -0300

    added routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './index.css';
 
-import { HomePage } from './pages/HomePage/HomePage'
-import { HomePageScroll } from './pages/HomePageScroll/HomePageScroll'
+import { HomePage } from './pages/HomePage/HomePage';
+import { HomePageScroll } from './pages/HomePageScroll/HomePageScroll';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HomePageScroll/>
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/scroll" element={<HomePageScroll />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);

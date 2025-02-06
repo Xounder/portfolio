4b3da3866dd704c13845cdb228cabbb6ef87cd32
Xounder <renanfellipe1@hotmail.com>
2025-02-06 07:32:07 -0300

    added media + modifies to default-card component

--- src/components/DefaultCard/DefaultCard.tsx
+++ src/components/DefaultCard/DefaultCard.tsx
@@ -6,18 +6,23 @@ export interface DefaultCardProps {
     image: { img:string, alt:string }
     title: string;
     description: string;
-    link: string;
+    link?: string;
     component?: ReactElement
 }
-export const DefaultCard = ({ image, title, description, component}: DefaultCardProps) => {
+export const DefaultCard = ({ image, title, description, link, component}: DefaultCardProps) => {
+    
     return (
-        <a className="default-card" href={'/'} target="_blank" rel="noopener noreferrer">
+        <a className="default-card" href={link} target="_blank" rel="noopener noreferrer">
             <div className='default-card__content'>
-                <img src={image.img} alt={image.alt} />
-                <div className='default-card__content__text'>
-                    <h3>{title}</h3>
-                    <p>{description}</p>
-                    <div className="default-card__component">
+                <div className='default-card__content__image'>
+                    <img src={image.img} alt={image.alt} />
+                </div>
+                <div className='default-card__content__'>
+                    <div className='default-card__content__text'>
+                        <h3>{title}</h3>
+                        <p>{description}</p>
+                    </div>
+                    <div className="default-card__content__component">
                         {component}
                     </div>
                 </div>

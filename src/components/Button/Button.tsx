9f9c16b6e61b789f71ef7404337465d5e14f7f68
Xounder <renanfellipe1@hotmail.com>
2025-02-06 07:27:35 -0300

    refactored button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,15 +2,13 @@ import './Button.css';
 
 type ButtonProps = {
     text: string;
-    size: [number, number];
-    fontSize: number;
     type: 'button' | 'submit';
     onClick: () => void;
 }
 
-export const Button = ({ text, size, fontSize, type,  onClick }: ButtonProps) => {
+export const Button = ({ text, type,  onClick }: ButtonProps) => {
     return (
-        <button type={type} className="button" onClick={onClick} style={{padding: `${size[0]}em ${size[1]}em`, fontSize: `${fontSize}em`}}>
+        <button type={type} className="button" onClick={onClick}>
             {text}
         </button>
     );

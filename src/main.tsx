c873f54f81fa042e4f341e065946f9acd43cc62c
Xounder <renanfellipe1@hotmail.com>
2025-02-11 06:55:31 -0300

    removed strict-mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
@@ -7,12 +6,10 @@ import { HomePage } from './pages/HomePage/HomePage';
 import { HomePageScroll } from './pages/HomePageScroll/HomePageScroll';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/scroll" element={<HomePageScroll />} />
       </Routes>
     </BrowserRouter>
-  </StrictMode>
 );
